c8151f9e6d18d016b11a8b660acb4e4cbabe38c3
Prajwal Adhikari <prajwalad101@gmail.com>
2023-02-06 08:32:13 +0545

    implement axios to fetch data based on params

--- client/src/features/search-business/hooks/useBusinesses.tsx
+++ client/src/features/search-business/hooks/useBusinesses.tsx
@@ -1,28 +1,46 @@
 import { FilterFields, IQueryData } from '@features/search-business/types';
-import {
-  buildBusinessQuery,
-  fetchBusinesses,
-} from '@features/search-business/utils/api';
+import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
+import { isString } from 'src/utils/text';
 
-type Props = {
+import axios from 'axios';
+
+export const fetchBusinesses = async (params: object) => {
+  const baseURL = process.env.NEXT_PUBLIC_HOST;
+
+  const response = await axios.get(`${baseURL}/api/business`, {
+    params,
+  });
+
+  console.log(response.data);
+  return response.data;
+};
+
+type UseBusinessesProps = {
   sort: string; // sort items based on this field(eg:-createdAt)
   filters: FilterFields; // filter items based on this property
   fields: string[]; // fields to select when fetching items
-  enabled: boolean; // only fetch if true,
+  enabled?: boolean; // only fetch if true,
 };
 
-function useBusinesses(props?: Partial<Props>) {
+function useBusinesses(props?: UseBusinessesProps) {
   const { sort, filters, fields, enabled } = props || {};
+  const {
+    query: { subcategory },
+  } = useRouter();
 
-  const queryURL = buildBusinessQuery(sort, filters, fields);
+  const params = {
+    sort,
+    fields: fields?.join(','),
+    ...(isString(subcategory) && { subcategory }),
+  };
 
   // if no enabled variable passed, enable automatic refetching
   const isEnabled = enabled === undefined ? true : enabled;
 
   const query = useQuery<IQueryData, Error>(
     ['business', sort, filters, fields],
-    () => fetchBusinesses(queryURL),
+    () => fetchBusinesses(params),
     {
       enabled: isEnabled, // only run when the filter button is clicked
       staleTime: 1000 * 10,
