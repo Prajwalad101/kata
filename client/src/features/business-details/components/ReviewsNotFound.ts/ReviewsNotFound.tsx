fc4827b0a0f53dab756282bb9528ffd191c687ad
Prajwal Adhikari <prajwalad101@gmail.com>
2023-02-11 09:18:07 +0545

    run animation when component is on view

--- client/src/features/business-details/components/ReviewsNotFound.ts/ReviewsNotFound.tsx
+++ client/src/features/business-details/components/ReviewsNotFound.ts/ReviewsNotFound.tsx
@@ -1,19 +1,30 @@
 import notFoundAnim from 'public/animations/no-data.json';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import Lottie from 'react-lottie';
 
 export default function ReviewsNotFound() {
-  const notFoundAnimOptions = {
+  const { ref, inView } = useInView();
+  const [isStopped, setIsStopped] = useState(true);
+
+  const animOptions = {
     loop: false,
     autoplay: true,
     animationData: notFoundAnim,
     rendererSetting: { preserveAspectRatio: 'xMidYMid slice' },
   };
 
+  useEffect(() => {
+    if (inView) {
+      setIsStopped(false);
+    }
+  }, [inView]);
+
   return (
-    <div>
-      <Lottie options={notFoundAnimOptions} height={300} />
+    <div ref={ref}>
+      <Lottie options={animOptions} isStopped={isStopped} height={300} />
       <p className="text-center text-xl font-medium text-gray-600">
-        Sorry, we could not find what you&apos;re looking for
+        Sorry. We could not find any reviews
       </p>
     </div>
   );
