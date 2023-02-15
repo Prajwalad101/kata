e5bec766b2012b3708532170e915d0b58158be0b
Prajwal Adhikari <prajwalad101@gmail.com>
2023-02-15 06:03:41 +0545

    modify ui on review card

--- client/src/features/write-review/components/ReviewCard/ReviewCard.tsx
+++ client/src/features/write-review/components/ReviewCard/ReviewCard.tsx
@@ -1,32 +1,54 @@
+import { IReview } from '@destiny/common/types';
 import Image from 'next/image';
 import RatingIcons from 'src/components/icons/ratings/RatingIcons';
+import Slider from 'src/components/slider/Slider';
+import { getRelativeDate } from 'src/utils/date';
+import { classNames } from 'src/utils/tailwind';
+import { getPublicFilePath } from 'src/utils/text';
 
-export default function ReviewCard() {
+interface ReviewCardProps {
+  review: IReview;
+}
+
+export default function ReviewCard({ review }: ReviewCardProps) {
+  const noImage = !review.images || review.images.length === 0;
+
   return (
-    <div className="h-[450px] overflow-hidden text-ellipsis rounded-md bg-gray-100 shadow-md transition-all hover:scale-[101%] hover:shadow-lg">
-      <div className="relative h-[250px]">
-        <Image
-          src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="review-image"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-md"
-        />
-      </div>
-      <div className="p-3">
+    <div
+      className={classNames(
+        'overflow-hidden text-ellipsis rounded-md bg-gray-100 shadow-md transition-all hover:scale-[101%] hover:shadow-lg sm:w-[350px]',
+        noImage ? 'h-min' : ''
+      )}
+    >
+      {review.images && review.images.length !== 0 && (
+        <Slider numItems={review.images.length} className="h-[280px] w-full">
+          {review.images.map((image, index) => (
+            <div key={index} className="relative h-full w-full">
+              <Image
+                src={getPublicFilePath(image)}
+                alt="image"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-l-sm"
+              />
+            </div>
+          ))}
+        </Slider>
+      )}
+      <div className={classNames(noImage ? 'px-3 py-6' : 'p-3')}>
         <div className="mb-3 flex justify-between">
           <h3 className="cursor-pointer text-lg font-medium leading-tight decoration-red-400 decoration-2 hover:underline">
             The Burger House
           </h3>
           <div>
-            <RatingIcons rating={4.5} />
+            <RatingIcons rating={review.rating} />
           </div>
         </div>
         <div className="mb-3 flex items-center justify-between">
-          <div className="group flex cursor-pointer items-center gap-2">
+          <div className="flex cursor-pointer items-center gap-2">
             <div className="h-[25px] shrink-0">
               <Image
-                src="https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+                src={review.author.picture}
                 alt="user profile image"
                 width={25}
                 height={25}
@@ -34,20 +56,22 @@ export default function ReviewCard() {
                 className="rounded-full"
               />
             </div>
-            <span className="inline-block text-gray-700 hover:underline group-hover:underline">
-              Milan shrestha
+            <span className="inline-block text-gray-700">
+              {review.author.userName}
             </span>
             {/* <span className="pl-1 inline-block text-gray-700">wrote</span> */}
           </div>
-          <span className="inline-block text-sm text-gray-500">5 min ago</span>
+          <span className="inline-block text-sm text-gray-500">
+            {getRelativeDate(review.createdAt)}
+          </span>
         </div>
-        <p className="line-clamp-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-          tempora possimus, explicabo distinctio provident velit quibusdam,
-          suscipit vel blanditiis sed dolores veniam voluptatibus cum qui quis
-          eum molestias perferendis ducimus maiores consectetur mollitia!
-          Nostrum necessitatibus nemo iure minima saepe earum consequatur magni!
-          Reiciendis nobis sequi debitis blanditiis, rerum dolores praesentium?
+        <p
+          className={classNames(
+            'leading-relaxed',
+            noImage ? 'line-clamp-15' : 'line-clamp-5'
+          )}
+        >
+          {review.review}
         </p>
       </div>
     </div>
--- client/src/features/write-review/components/ReviewCards/ReviewCards.tsx
+++ client/src/features/write-review/components/ReviewCards/ReviewCards.tsx
@@ -1,13 +1,16 @@
-import { ReviewCard } from '@features/write-review/components';
+import { useReviews } from '@features/business-details/queries';
+import ReviewCardSkeleton from '@features/write-review/ReviewCardSkeleton/ReviewCardSkeleton';
+import ReviewCard from '../ReviewCard/ReviewCard';
 
 export default function ReviewCards() {
-  const items = Array.from(Array(10).keys());
+  const { data, isLoading, isSuccess } = useReviews({ sort: '-createdAt' });
 
+  if (isLoading) return <div>loading ...</div>;
   return (
-    <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 md:gap-y-14 lg:grid-cols-3">
-      {items.map((_, key) => (
-        <ReviewCard key={key} />
-      ))}
+    <div className="mb-12 grid min-w-0 grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 md:gap-y-14 lg:grid-cols-3">
+      {isLoading && <ReviewCardSkeleton />}
+      {isSuccess &&
+        data.map((review, key) => <ReviewCard review={review} key={key} />)}
     </div>
   );
 }
