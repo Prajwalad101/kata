12022bd94bb8063936b6d7740bdbcdd3968192ae
Prajwal Adhikari <prajwalad101@gmail.com>
2023-02-18 11:08:38 +0545

    create api query to fetch questions

---
+++ client/src/features/business-details/queries/useQuestions.ts
@@ -0,0 +1,32 @@
+import { IQuestion } from "@destiny/common/types";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosInstance } from "axios";
+import { useRouter } from "next/router";
+import useCreateApi from "src/api/useCreateApi";
+
+interface ResponseData {
+  status: string;
+  documentCount: number;
+  data: IQuestion[]
+}
+
+const getQuestions = async (params: object, api: AxiosInstance) => {
+  const response = await api.get<ResponseData>('/questions', {
+    params
+  }); 
+  return response.data;
+}
+
+export default function useQuestions(queryParams: object){
+  const business = useRouter().query.businessId;
+  const newParams = {...queryParams, business};
+
+  const api = useCreateApi();
+
+  const query = useQuery({
+    queryKey: ['questions', newParams],
+    queryFn: () => getQuestions(newParams, api),
+  },
+  )
+  return query
+}
