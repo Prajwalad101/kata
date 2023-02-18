3a23854f6579662c3f890dab2b131d532b079c97
Prajwal Adhikari <prajwalad101@gmail.com>
2023-02-18 11:10:09 +0545

    create form to post user question on a business

--- client/src/features/business-details/components/PostQuestion/PostQuestion.tsx
+++ client/src/features/business-details/components/PostQuestion/PostQuestion.tsx
@@ -1,45 +1,93 @@
-import { useState } from 'react';
+import { isString } from '@destiny/common/utils';
+import useSubmitQuestion from '@features/business-details/queries/useSubmitQuestion';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import { PrimaryButton, SecondaryButton } from 'src/components';
+import FormErrorMessage from 'src/components/FormErrorMessage/FormErrorMessage';
+import { useUser } from 'src/layouts/UserProvider';
+
+interface FormInputs {
+  question: string
+}
+
+const defaultValues: FormInputs = {
+  question: ''
+}
 
 interface PostQuestionProps {
   closeDialog: () => void;
 }
 
 export default function PostQuestion({ closeDialog }: PostQuestionProps) {
-  const [question, setQuestion] = useState('');
+  const business = useRouter().query.businessId;
+  const user = useUser();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+  const {register, watch, handleSubmit, formState, reset} = useForm({defaultValues});
+  const submitQuestionMutation = useSubmitQuestion();
+
+  const onSubmit = (data: FormInputs) => {
+    // check if author and business exists
+    if(user?._id && isString(business)){
+      submitQuestionMutation.mutate({
+        question: data.question,
+        business,
+        author: user?._id
+      })
+    }
+  }
+
+  useEffect(() => {
+    if(submitQuestionMutation.isSuccess){
+      reset(defaultValues)
+      toast.success('Successfully posted question');
+      closeDialog(); 
+    }
+  },[submitQuestionMutation.isSuccess])
+
+  const question = watch('question')
+  console.log(user?.userName);
+  
 
   return (
-    <form className="mb-12" onSubmit={handleSubmit}>
+    <form className="mb-12" onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-5 flex items-center gap-5">
         <div className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-blue-500 text-white">
           <span>A</span>
         </div>
-        <p>Anonymous</p>
+        <p>{user?.userName}</p>
       </div>
 
       <div>
         <textarea
           id="question"
+          {...register('question', {
+            maxLength: {
+              value: 50,
+              message: 'Question is too long' 
+            },
+            minLength: {
+              value: 5,
+              message: 'Question is too short'
+            },
+          })}
           rows={7}
           className="mb-3 w-full rounded-md bg-gray-200 py-4 px-5 ring-inset ring-blue-500 focus:outline-none focus:ring"
           placeholder="Write your question"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
         />
         <p className="text-right text-sm text-gray-600">
           {question.length} / 200
         </p>
       </div>
-
+      <FormErrorMessage error={formState.errors.question} />
       <div className="mb-8 flex flex-wrap gap-8">
         <SecondaryButton className="px-8 py-2" onClick={closeDialog}>
           Cancel
         </SecondaryButton>
-        <PrimaryButton className="px-10 py-2" type="submit">
+        <PrimaryButton isLoading={submitQuestionMutation.isLoading}
+          className="px-10 py-2"
+          type="submit">
           Post
         </PrimaryButton>
       </div>
