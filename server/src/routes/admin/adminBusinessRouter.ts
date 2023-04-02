7c76d6ad9d7bd913cc277b44a7d9f044886977e5
Prajwal Adhikari <prajwalad101@gmail.com>
2023-04-02 13:07:46 +0545

    create router for admin business

--- server/src/routes/admin/adminBusinessRouter.ts
+++ server/src/routes/admin/adminBusinessRouter.ts
@@ -1,11 +1,18 @@
 // create express router
 import express from 'express';
+import {
+  getBusiness,
+  getAllBusinesses,
+} from '../../controllers/admin/adminBusinessController';
 import businessController from '../../controllers/businessController';
 
 const router = express.Router();
 
-router.route('/').get(businessController.getAllBusinesses);
-
-router.route('/show/:id').get(businessController.getBusiness);
+router.route('/').get(getAllBusinesses);
+router
+  .route('/:id')
+  .get(getBusiness)
+  .patch(businessController.updateBusiness)
+  .delete(businessController.deleteBusiness);
 
 export default router;
